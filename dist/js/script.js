1b46ba1c207c760a1d8ab45259e9b95a21c511c6
rekaawij <wijayareka02@gmail.com>
2023-08-20 15:16:25 +0700

    update responsive

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,15 @@
+// navbar fixed
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
+
 // hamburger//
 const hamburger = document.querySelector("#hamburger");
 const navmenu = document.querySelector("#nav-menu");
